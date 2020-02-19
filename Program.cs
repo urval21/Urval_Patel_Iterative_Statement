f58ff2ba5ba3f535245b2bbe7c6ba397e8a1b383
urval <urval21@gmail.com>
2020-02-19 11:49:08 -0500

    Version 2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace Urval_Patel_Iterative_Statement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter an integer value between 2 and 10:");
+            
+            int i = Convert.ToInt32(Console.ReadLine());
+            int j;
+
+            if (i%2==0)
+            {
+                i++;
+                for (j = 0; j < i; j++)
+                {
+                    Console.WriteLine("You entered an even number");
+                }
+            }
+
+
+            else
+            {
+                i--;
+                for (j = 0; j < i; j++)
+
+                {
+                    Console.WriteLine("You entered an odd number");
+                }
+                
+            }           
         }
     }
 }
